39e55855762553d3bd2f6069ee1927f78b80d8bd
vinays23 <125316412+vinays23@users.noreply.github.com>
2023-02-21 16:05:44 -0800

    Update BinHeap.py

--- BinHeap.py
+++ BinHeap.py
@@ -1,7 +1,7 @@
 # Requirement: Build a minheap wihout using Python delivered functions; using a customizied binary tree structure where each node in the tree is less than or equal to the child nodes.
 # Solution: Create a customized binary tree structure.  
 
-# HOW DID I CONSTRUCT THE SOLUTION:
+# HOW I CONSTRUCTED THE SOLUTION:
 
 class BinHeap:
 
@@ -27,6 +27,8 @@ class BinHeap:
         self.heapList.append(k)
         self.currentSize = self.currentSize + 1
         self.percUp(self.currentSize)
+        
+# STEP 4: Function 4 - Moves a node down the tree structure if it is greater than its children's value.         
 
     def percDown(self,i):
         while (i * 2) <= self.currentSize:
@@ -36,6 +38,9 @@ class BinHeap:
                 self.heapList[i] = self.heapList[mc]
                 self.heapList[mc] = tmp
             i = mc
+            
+# STEP 5: Function 5 - Returns the location of the smallest child at position i.
+
     def minChild(self,i):
         if i * 2 + 1 > self.currentSize:
             return i * 2
@@ -44,6 +49,9 @@ class BinHeap:
                 return i * 2
             else:
                 return i * 2 + 1
+            
+# STEP 6: Function 6 - Removes and subsequently returns the smallest value in the heap.
+
     def delMin(self):
         retval = self.heapList[1]
         self.heapList[1] = self.heapList[self.currentSize]
@@ -51,6 +59,9 @@ class BinHeap:
         self.heapList.pop()
         self.percDown(1)
         return retval
+    
+# STEP 7: Function 7 - Creates a heap from a given list of values.
+    
     def buildHeap(self,alist):
         i = len(alist) // 2
         self.currentSize = len(alist)
@@ -59,6 +70,8 @@ class BinHeap:
             self.percDown(i)
             i = i - 1
 
+# STEP 8: Function 8 - Using previously defined functions, this sorts the heap.             
+            
     def heapSort(self):
         alist = []
 
@@ -66,24 +79,12 @@ class BinHeap:
             alist.append(self.delMin())
         self.buildHeap(alist)
 
+# STEP 9: Function 9 - Prints the sorted heap        
+        
     def printList(self):
         print(self.heapList[1:])
 
 
-#a = [1, 2, 5, 3, 6, 9, 8, 25, 20]
-
-bh = BinHeap()
-
-
-bh.insert(5)
-bh.insert(8)
-bh.insert(6)
-bh.insert(2)
-bh.insert(12)
-bh.insert(11)
-
-bh.heapSort()
-bh.printList()
 
 
 
