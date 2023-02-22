d98fc033d6db21202bc985a0f7be52cf2a2ade47
vinays23 <125316412+vinays23@users.noreply.github.com>
2023-02-21 17:58:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,40 @@ def outputRoster():
         print (f"Jersey number: {jerseyNumber}, Rating: {teamRoster[jerseyNumber]}")
     print()
 def addPlayer():
-    jerseyNumber = int(input("Enter a new player's jersey number:\n"))
-    teamRoster[jerseyNumber] = int(input("Enter the player's rating:\n"))
-    print()
+    try:
+        jerseyNumber = int(input("Enter a new player's jersey number:\n"))
+        teamRoster[jerseyNumber] = int(input("Enter the player's rating:\n"))
+    except ValueError:
+        print('Input is not an integer. Please input an integer')
+    else:
+        print('The player number and rating has been added.')
+    print()    
 def deletePlayer():
-    jerseyNumber = int(input("Enter a jersey number:\n"))
-    teamRoster.pop(jerseyNumber)
+    try:
+        jerseyNumber = int(input("Enter a jersey number:\n"))
+        teamRoster.pop(jerseyNumber)
+    except KeyError:
+        print('Invalid jersey number. Please enter a valid jersey number.')
+    else:
+        print('The player has been deleted.')
     print()
 def updatePlayerRating():
-    jerseyNumber = int(input("Enter a jersey number:\n"))
-    teamRoster[jerseyNumber] = int(input("Enter a new rating for player:\n"))
+    try:
+        jerseyNumber = int(input("Enter a jersey number:\n"))
+        teamRoster[jerseyNumber] = int(input("Enter a new rating for player:\n"))
+    except ValueError:
+        print('Input is not an integer. Please input an integer.')
+    except KeyError:
+        print('Invalid jersey number. Please enter a valid jersey number')
+    else:
+        print('The player rating has been updated.')
     print()
 def outputPlayersAboveRating():
-    givenRating = int(input("Enter a rating:\n") )
+    try:
+        givenRating = int(input("Enter a rating:\n") )
+    except ValueError:
+        print('Input is not an integer. Please input an integer.')
+        return    
     print()
     print ("ABOVE",givenRating)
     for jerseyNumber in sorted(teamRoster.keys() ):
@@ -65,4 +86,6 @@ while True:
     if option == 'o':
         outputRoster()
         continue
+    else:
+        print('Invalid option. Please input a key based on the menu.')
     
