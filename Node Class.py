a2809d21ccfdf884f1dba51dd9b673dee4a2c597
vinays23 <125316412+vinays23@users.noreply.github.com>
2023-02-22 13:22:32 -0800

    Update and rename hw3p2CLEAN.py to Node Class.py

--- Node Class.py
+++ Node Class.py
@@ -1,25 +1,42 @@
+#GOAL: Modify and manipulate a node within a linked list without using Python's built-in functions.
+
 class Node:
+    
+    #Function 1: Initialize the class.
     def __init__(self,initdata):
         self.data = initdata
         self.next = None
 
+    #Function 2: Return the value of the node.
+    
     def getData(self):
         return self.data
 
+    #Function 3: Return the value of the next attribute of the current node.
+    
     def getNext(self):
         return self.next
 
+    #Function 4: Set the value of a node to a new value.
+    
     def setData(self,newdata):
         self.data = newdata
 
+    #Function 5: Set the value of the next node to a new value.
+    
     def setNext(self,newnext):
         self.next = newnext
 
+        
+#Function 6: Traverses through the linked list to see if there is a specific node value within the list.        
+        
 def findValue(val, node):
 
-#if value matches node, return true.
+    #If value matches node, return true.
+    
     if node.getData() == val:
         return True
+
 
     if node.getNext() == None:
         return False
@@ -28,13 +45,7 @@ def findValue(val, node):
 
     return found_val
 
-
-
-
-
-
 #if value does not match node, delegate the task to the next node.
-
 
     if node.getNext() != None:
         
@@ -48,15 +59,3 @@ def findValue(val, node):
     return found_val
 
     
-#    print(findValue(node.getNext(), val))
-    
-
-n1 = Node(10)
-n2 = Node(20)
-n3 = Node(30)
-n4 = Node(40)
-n2.setNext(n1)
-n3.setNext(n2)
-n4.setNext(n3)
-
-print(findValue(40, n2))
