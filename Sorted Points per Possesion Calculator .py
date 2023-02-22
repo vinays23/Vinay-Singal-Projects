283b0b55bd159a31bd31e14fa78cb996697f26dc
vinays23 <125316412+vinays23@users.noreply.github.com>
2023-02-22 13:08:34 -0800

    Update Sorted Points per Possesion Calculator .py

--- Sorted Points per Possesion Calculator .py
+++ Sorted Points per Possesion Calculator .py
@@ -1,28 +1,44 @@
+# Objective: Create a viable points per possession calculator for a basketball team of 12 players, and rank the players based on their ppp value.
+
+# STEP 1: Initialize a dictionary.
+
 pos_efficiency = {}
 
-i = 1
+#STEP 2: Create a loop which runs 12 times.
 
-for i in range (1,13):
-    jersey_num = int(input("Enter player {}'s jersey number:\n".format(i)))
-    PTS = int(input('Enter points scored for player {}: '.format(i)))
-    FGA = int(input('Enter field goals attempted for player {}: '.format(i)))
-    FTA = int(input('Enter free throws attempted for player {}: '.format(i)))
-    TO = int(input('Enter turnovers committed by player {}: '.format(i)))
-    PPP = PTS/(FGA + (0.44 * FTA) + TO)
+for i in range(1, 13):
+    while True:
+        
+        #STEP 3: Force the user to make the correct inputs.
+        
+        try:
+            jersey_num = int(input("Enter player {}'s jersey number (0-99): ".format(i)))
+            if jersey_num < 0 or jersey_num > 99:
+                raise ValueError("Invalid jersey number, please enter a number between 0 and 99.")
+            
+            pts = int(input("Enter points scored for player {}: ".format(i)))
+            fga = int(input("Enter field goals attempted for player {}: ".format(i)))
+            fta = int(input("Enter free throws attempted for player {}: ".format(i)))
+            to = int(input("Enter turnovers committed by player {}: ".format(i)))
+            
+            #STEP 4: Calculate their ppp value based on the information provided.  
+            
+            ppp = pts / (fga + (0.44 * fta) + to)
+            pos_efficiency[ppp] = jersey_num
+            
+            break  
+        
+        #STEP 5: Display the error in case of incorrect inputs.
+        
+        except ValueError as e:
+            print(e)  
+            continue  
 
-    if jersey_num < 0 or jersey_num > 99:
-        break
-    else:
-        pos_efficiency[PPP] = jersey_num
-
-
-
-sorted_roster = sorted(pos_efficiency.items(), reverse = True)
+#STEP 6: Sort the roster based on PPP and display the full report.            
+            
+sorted_roster = sorted(pos_efficiency.items(), reverse=True)
 
 print()
-for PPP, jersey_num in sorted_roster:
-
-    rounded_PPP = round(PPP,2) 
-
-    print('Jersey number: {}, Points per Possession: {}'.format(jersey_num, rounded_PPP))
-
+for ppp, jersey_num in sorted_roster:
+    rounded_ppp = round(ppp, 2)
+    print("Jersey number: {}, Points per Possession: {}".format(jersey_num, rounded_ppp))
